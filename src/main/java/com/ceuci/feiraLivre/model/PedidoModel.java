608454251f9e872812ca19ae7b8076d82734ff2d
Victor Tavares <victor.as156@gmail.com>
2020-11-17 20:44:47 -0300

    Configurando segurança de Usuario

--- src/main/java/com/ceuci/feiraLivre/model/PedidoModel.java
+++ src/main/java/com/ceuci/feiraLivre/model/PedidoModel.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -77,5 +79,7 @@ public class PedidoModel {
 	public void setUsuario(UsuarioModel usuario) {
 		this.usuario = usuario;
 	}
+	
+	
 	
 }
--- src/main/java/com/ceuci/feiraLivre/repository/UsuarioRepository.java
+++ src/main/java/com/ceuci/feiraLivre/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.ceuci.feiraLivre.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import com.ceuci.feiraLivre.model.UsuarioModel;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
 	public List<UsuarioModel> findAllByNomeContainingIgnoreCase (String nome);
+	
+	public Optional<UsuarioModel> findByEmail (String email);
 }
